cf5c1d912ea4ec7e872bac1be6f7d07ba7c15f40
Misaki <45246543+kageroukw@users.noreply.github.com>
2020-09-20 14:23:45 +0200

    Added creds

---
+++ Services/CredentialSetup.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace JishoBot.Services
+{
+    public class CredentialSetup
+    {
+        public string Prefix { get; }
+        
+        public string Token { get; }
+
+        public CredentialSetup()
+        {
+            if (!File.Exists("/Data/credentials.json"))
+            {
+                Console.WriteLine($"credentials.json not found!");
+                Environment.Exit(1);
+            }
+
+            var config = new ConfigurationBuilder();
+            config.AddJsonFile(Path.Combine(Directory.GetCurrentDirectory(), "/Data/credentials.json"));
+
+            var data = config.Build();
+            Prefix = data[nameof(Prefix)];
+            Token = data[nameof(Token)];
+        }
+    }
+}
