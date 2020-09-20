8109206ba50a1656071120c21af76ebf431453f8
Misaki <45246543+kageroukw@users.noreply.github.com>
2020-09-20 14:24:06 +0200

    Initialized

--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
+using Disqord.Bot.Prefixes;
+using Disqord;
+using Disqord.Bot.Extended;
+using Disqord.Bot;
+using JishoBot.Services;
 
 namespace JishoBot
 {
-    class Program
+    public class Program
     {
         public static Task Main(string[] args)
         {
@@ -15,9 +20,23 @@ namespace JishoBot
 
         private static IServiceProvider BuildServiceProvider()
         {
+            var _creds = new CredentialSetup();
             return new ServiceCollection()
                 .AddSingleton<Jisho>()
-                .AddSingleton(_ => new )
+                .AddSingleton(_ => new DefaultPrefixProvider().AddPrefix(_creds.Prefix).AddMentionPrefix())
+                .AddSingleton(container => new DiscordBotConfiguration
+                {
+                    Logger = new ExtendedSimpleLogger(new ExtendedSimpleLoggerConfiguration
+                    {
+                        EnableInformationLogSeverity = true
+                    }),
+
+                    ProviderFactory = _ => container
+                })
+                .AddSingleton(container => new DiscordBot(TokenType.Bot, _creds.Token,
+                    container.GetRequiredService<DefaultPrefixProvider>(),
+                    container.GetRequiredService<DiscordBotConfiguration>()))
+                .BuildServiceProvider();
         }
     }
 }
