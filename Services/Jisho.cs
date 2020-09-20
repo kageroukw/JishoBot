515181f71b9ea13760d8c6b0361ff213aa264e7f
Misaki <45246543+kageroukw@users.noreply.github.com>
2020-09-20 14:24:00 +0200

    Added main bot file

---
+++ Services/Jisho.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Disqord.Bot;
+
+namespace JishoBot
+{
+    public class Jisho
+    {
+        private readonly DiscordBot _jisho;
+
+        public Jisho(DiscordBot jisho)
+        {
+            _jisho = jisho;
+        }
+
+        public async Task InitializeAsync()
+        {
+            _jisho.AddModules(Assembly.GetEntryAssembly());
+
+            await _jisho.RunAsync();
+        }
+    }
+}
