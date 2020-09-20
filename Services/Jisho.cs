157ee6a90399055f6387c0c9c8e1bc63b0482f00
Misaki <45246543+kageroukw@users.noreply.github.com>
2020-09-20 14:33:29 +0200

    Added ISearch

--- Services/Jisho.cs
+++ Services/Jisho.cs
@@ -4,16 +4,19 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Disqord.Bot;
+using JishoBot.Services.Search;
 
 namespace JishoBot
 {
     public class Jisho
     {
         private readonly DiscordBot _jisho;
+        private readonly ISearch _search;
 
-        public Jisho(DiscordBot jisho)
+        public Jisho(DiscordBot jisho, ISearch search)
         {
             _jisho = jisho;
+            _search = search;
         }
 
         public async Task InitializeAsync()
