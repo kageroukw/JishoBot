d603cb538973be2b1d00983551ba8dadeeb51ac7
Misaki <45246543+kageroukw@users.noreply.github.com>
2020-09-20 13:59:36 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace JishoBot
+{
+    class Program
+    {
+        public static Task Main(string[] args)
+        {
+            var container = BuildServiceProvider();
+            var bot = container.GetRequiredService<Jisho>();
+            return bot.InitializeAsync();
+        }
+
+        private static IServiceProvider BuildServiceProvider()
+        {
+            return new ServiceCollection()
+                .AddSingleton<Jisho>()
+                .AddSingleton(_ => new )
+        }
+    }
+}
